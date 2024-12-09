9f6762cd9730539eac9766f10c5cf33cc8cb924f
fourcheerv <fourches.erv@gmail.com>
2024-12-09 06:58:41 +0100

    Update test_email.js

--- test_email.js
+++ test_email.js
@@ -4,18 +4,18 @@
 import emailjs from 'emailjs-com';
 
 // ID de votre service (obtenu dans EmailJS)
-const SERVICE_ID = 'votre_service_id';
+const SERVICE_ID = 'service_colis_smtp';
 // ID de votre modèle d'e-mail (défini dans EmailJS)
 const TEMPLATE_ID = 'votre_template_id';
 // Clé utilisateur (trouvée dans votre tableau de bord EmailJS)
-const USER_ID = 'votre_user_id';
+const USER_ID = 'UFlNoLfp7PdWyrBak';
 
 function envoyerEmail() {
   const params = {
-    from_name: 'Votre nom',
-    to_name: 'Nom du destinataire',
+    from_name: 'POPO',
+    to_name: 'spokorski@gmail.com',
     message: 'Ceci est un message de test',
-    reply_to: 'votre@email.com',
+    reply_to: 'sebastien.pokorski@estrepublicain.fr',
   };
 
   emailjs
