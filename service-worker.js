6ec9156b941f6fd4d8db5be9f8df77d7b642fe3e
fourcheerv <fourches.erv@gmail.com>
2024-12-15 22:54:46 +0100

    Update

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "remote-colis-cache-v1";
+const urlsToCache = [
+  "/remote_colis/",
+  "/remote_colis/index.html",
+  "/remote_colis/style.css",
+  "/remote_colis/script.js",
+  "/remote_colis/icons/icon-192x192.png",
+  "/remote_colis/icons/icon-512x512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
